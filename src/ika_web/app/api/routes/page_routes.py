ca44a8b7bae8ed38ef45b2cee1d85a05f7fb8178
Manitra RANAIVOHARISON <mranaivoharison@gmail.com>
2020-05-01 18:18:02 +0200

    [master]: add login

--- src/ika_web/app/api/routes/page_routes.py
+++ src/ika_web/app/api/routes/page_routes.py
@@ -2,7 +2,7 @@ import os
 import flask
 import json
 import requests
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 app = flask.Blueprint("page", __name__)
 
 @app.route("/")
@@ -13,7 +13,7 @@ def login_page():
     flask.flash("You were successfully logged in")
 
     return flask.render_template(
-        "index.html",
+        "login_ika.html",
         redirect_url=os.environ.get("FN_BASE_URI", default=False) + "/api/v1/google/authorize",
         registrer=os.environ.get("FN_BASE_URI", default=False) + "/registrer"
     )
@@ -22,39 +22,55 @@ def login_page():
 def registrer():
     return flask.render_template("registrer.html")
 
-@app.route("/add_registrer", methods=['GET', 'POST'])
-def validate():
+@app.route("/add_registrer", methods=['GET','POST'])
+def add_registrer():
     url=os.environ.get("FN_BASE_URI", default=False) + "/api/v1/auth/signup"
     if request.method == 'POST':
-        form = request.form
-        print(form)
-        myobj = {
-                'email':request.form['your_email'],
-                'password':request.form['password'],
-                }
-        x = requests.post(url, json=myobj)
-    return flask.render_template("index.html")
+        if request.form['password'] == request.form['comfirm_password']:
+            myobj = {
+                    'email':request.form['your_email'],
+                    'password':request.form['password'],
+                    'first_name':request.form['first_name'],
+                    'last_name':request.form['last_name']
+                    }
+            x = requests.post(url, json=myobj)
+        else:
+            print('mdp diff')
+            return flask.render_template("registrer.html")
+        
+    return flask.render_template("login_ika.html")
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
+
+@app.route("/login_ika", methods=['GET','POST'])
+def login_ika():
     url=os.environ.get("FN_BASE_URI", default=False) + "/api/v1/auth/login"
-    if request.method == 'POST':
-        form = request.form
+    url_test=os.environ.get("FN_BASE_URI", default=False) + "/loading_page"
+    
+    if request.method == 'GET':
+            return render_template('login_ika.html')
+        
+    elif request.method == 'POST':
         myobj = {
         'email':request.form['email'],
         'password':request.form['pass'],
         }
         r = requests.post(url, json=myobj)
-        data = r.json() 
+        data = r.json()
         print(data)
-    return flask.render_template("login.html") 
+        
+        
+        return redirect("/api/v1/google/authorize")
+        
+    else:
+        print('error')
+    
     
 @app.route("/home")
 def home_page():
     """
     Returns:
     """
-    return flask.render_template("home.html", error=error)
+    return flask.render_template("home.html")
 
 
 @app.route("/loading_page")
@@ -62,4 +78,4 @@ def loading_page():
     """
     Returns:
     """
-    return flask.render_template("login.html")
+    return flask.render_template("login_google.html")
