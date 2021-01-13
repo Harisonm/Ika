243ceab532007ee4fb05b205e1125fab5d3d325b
Manitra RANAIVOHARISON <mranaivoharison@gmail.com>
2021-01-13 15:48:49 +0100

    [master]: add terraform config

---
+++ terraform-config/outputs.tf
@@ -0,0 +1,8 @@
+output "endpoint" {
+  value = google_container_cluster.default.endpoint
+}
+
+output "master_version" {
+  value = google_container_cluster.default.master_version
+}
+
---
+++ terraform-config/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  default = "demo-cluster"
+}
+
+variable "project" {
+  default = "formation-mac-298810"
+}
+
+variable "location" {
+  default = "us-central1"
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
